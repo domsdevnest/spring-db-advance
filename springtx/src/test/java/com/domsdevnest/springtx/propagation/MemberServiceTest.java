fc8a9971052247836791d3d9fa7398c9c6c14a7d
domsdevnest <domsdevnest@gmail.com>
2025-05-01 21:46:00 +0900

    트랜잭션 예외

--- springtx/src/test/java/com/domsdevnest/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/com/domsdevnest/springtx/propagation/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -21,7 +22,6 @@ class MemberServiceTest {
      * memberRepository @Transactional : ON
      * logRepository    @Transactional : ON
      */
-
     @Test
     void outerTxOff_success() {
         String username = "outerTxOff_success";
@@ -32,4 +32,19 @@ class MemberServiceTest {
 
     }
 
+    /**
+     * memberService    @Transactional : OFF
+     * memberRepository @Transactional : ON
+     * logRepository    @Transactional : ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        String username = "로그예외_outerTxOff_fail";
+
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
 }
